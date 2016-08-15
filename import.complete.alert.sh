3a116c9f3ad978f4c45765c5a18f32b210227cce
lstrand <leonstrand@gmail.com>
2016-08-15 07:59:35 -0700

    added stage comments and output

--- import.complete.alert.sh
+++ import.complete.alert.sh
@@ -13,10 +13,20 @@ check() {
   container_notified=0
   netcat_notified=0
   curl_notified=0
+  stage_1_announced=0
+  stage_2_announced=0
+  stage_3_announced=0
+  stage_4_announced=0
   while :; do 
+    # stage 1 of 4: containers
+    if [ $stage_1_announced -eq 0 ]; then
+      echo
+      echo
+      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): stage 1 of 4: containers
+      stage_1_announced=1
+    fi
     containers_up=$(docker ps -q -f 'name=dockerelk_*' | wc -l)
     if [ "$containers_up" -lt $containers_up_threshold ]; then
-      echo
       echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): $containers_up of $containers_up_threshold containers up
       echo docker ps -af name=dockerelk_\\*
       docker ps -af name=dockerelk_\*
@@ -24,19 +34,23 @@ check() {
       continue
     fi
     if [ $container_notified -ne 1 ]; then
-      echo
       echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): all containers up
       echo docker ps -af name=dockerelk_\\*
       docker ps -af name=dockerelk_\*
       container_notified=1
+    fi
+    # stage 2 of 4: elasticsearch cluster
+    if [ $stage_2_announced -eq 0 ]; then
       echo
+      echo
+      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): stage 2 of 4: elasticsearch cluster
+      stage_2_announced=1
     fi
     if nc -w1 localhost 9200; then
       if [ $netcat_notified -ne 1 ]; then
         echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): connection to localhost:9200 succeeded
         echo nc -vw1 localhost 9200
         nc -vw1 localhost 9200
-        echo
         netcat_notified=1
       fi
     else
@@ -45,21 +59,32 @@ check() {
       sleep 1
       continue
     fi
+    # stage 3 of 4: elasticsearch indices
+    if [ $stage_3_announced -eq 0 ]; then
+      echo
+      echo
+      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): stage 3 of 4: elasticsearch indices
+      stage_3_announced=1
+    fi
     if curl --connect-timeout 1 localhost:9200/_cat/indices?v 1>/dev/null 2>&1; then
       if [ $curl_notified -ne 1 ]; then
-        #echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): curl --connect-timeout 1 localhost:9200/_cat/indices?v
         echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): elasticsearch indices curl succeeded
         echo curl --connect-timeout 1 localhost:9200/_cat/indices?v
         curl --connect-timeout 1 localhost:9200/_cat/indices?v
         curl_notified=1
-        echo
-        echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): checking cpu usage
       fi
     else
       echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): elasticsearch indices curl failed
       echo curl --connect-timeout 1 localhost:9200/_cat/indices?v
       sleep 1
       continue
+    fi
+    # stage 4 of 4: cpu usage
+    if [ $stage_4_announced -eq 0 ]; then
+      echo
+      echo
+      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): stage 4 of 4: cpu usage
+      stage_4_announced=1
     fi
     cpu_usage_threshold_exceeded=0
     cpu_usages=$(top -bn1 | egrep 'docker-compose|jvm' | grep -v grep | awk '{print $9}')
