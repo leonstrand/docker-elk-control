35f22644966951ea7d5e080fdc387446d893f839
lstrand <leon.strand@medeanalytics.com>
2016-09-02 14:03:51 -0700

    added check for unchanged elasticsearch configuration template

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -5,9 +5,17 @@
 
 if [ -n "$ELASTICSEARCH_INDICES_PATH" ]; then
   echo $0: elasticsearch indices path: $ELASTICSEARCH_INDICES_PATH
-  command="cd ~/docker-elk && time docker-compose up"
-  echo $command
-  eval $command
+  if grep -q 'network.publish_host: docker host ip address or hostname' ~/docker-elk/elasticsearch/config/elasticsearch.yml; then
+    echo $0: error: can not start elk
+    echo $0: error: elasticsearch configuration lacks required network information
+    echo $0: set like so:
+    echo ~/docker-elk-control/startup.sh
+    echo $0: then rerun $0
+  else
+    command="cd ~/docker-elk && time docker-compose up"
+    echo $command
+    eval $command
+  fi
 else
   echo $0: error: can not start elk
   echo $0: error: elasticsearch indices path unknown
