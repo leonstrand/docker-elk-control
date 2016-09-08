a0b8ae2923b6f758914b88c7ff99de7fbe7b0999
lstrand <leon.strand@medeanalytics.com>
2016-09-08 10:13:38 -0700

    split out config file check and start command added environment variable set if undefined but expected directory exists

--- docker-compose-up.sh
+++ docker-compose-up.sh
@@ -3,8 +3,9 @@
 # leon.strand@medeanalytics.com
 
 
-if [ -n "$ELASTICSEARCH_INDICES_PATH" ]; then
-  echo $0: elasticsearch indices path: $ELASTICSEARCH_INDICES_PATH
+directory=/elk
+
+start() {
   if grep -q 'network.publish_host: docker host ip address or hostname' ~/docker-elk/elasticsearch/config/elasticsearch.yml; then
     echo $0: error: can not start elk
     echo $0: error: elasticsearch configuration lacks required network information
@@ -16,9 +17,22 @@ if [ -n "$ELASTICSEARCH_INDICES_PATH" ]; then
     echo $command
     eval $command
   fi
+}
+
+if [ -n "$ELASTICSEARCH_INDICES_PATH" ]; then
+  echo $0: info: elasticsearch indices path environment variable ELASTICSEARCH_INDICES_PATH defined
+  echo $0: info: elasticsearch indices path: $ELASTICSEARCH_INDICES_PATH
+  start
 else
-  echo $0: error: can not start elk
-  echo $0: error: elasticsearch indices path unknown
-  echo $0: set like so:
-  echo export ELASTICSEARCH_INDICES_PATH=/elk
+  if [ -d /elk ]; then
+    echo $0: warning: elasticsearch indices path environment variable ELASTICSEARCH_INDICES_PATH undefined but directory $directory exists
+    echo $0: warning: setting elasticsearch indices path environment variable ELASTICSEARCH_INDICES_PATH to $directory
+    export ELASTICSEARCH_INDICES_PATH=/elk
+    start
+  else
+    echo $0: error: can not start elk
+    echo $0: error: elasticsearch indices path undefined and directory $directory does not exist
+    echo $0: set like so:
+    echo export ELASTICSEARCH_INDICES_PATH=/elk
+  fi
 fi
