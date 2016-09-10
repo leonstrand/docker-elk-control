799d512b87c14b8ac8e5b456a970cd5bc0d1ea7d
lstrand <leon.strand@medeanalytics.com>
2016-09-10 11:38:04 -0700

    abstracted process and string

--- cpu.idle.alert.sh
+++ cpu.idle.alert.sh
@@ -3,6 +3,8 @@
 # leon.strand@medeanalytics.com
 
 
+process='jvm'
+string=$process' cpu usage'
 cpu_usage_threshold=7
 check_passed_threshold=15
 alerts=10
@@ -14,20 +16,20 @@ check() {
     if [ $stage_4_announced -eq 0 ]; then
       echo
       echo
-      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): stage 4 of 4: cpu usage
+      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): $string
       stage_4_announced=1
     fi
     cpu_usage_threshold_exceeded=0
-    cpu_usages=$(top -bn1 | egrep 'docker-compose|jvm' | grep -v grep | awk '{print $9}')
+    cpu_usages=$(top -bn1 | grep $process | grep -v grep | awk '{print $9}')
     if [ -z "$cpu_usages" ]; then
-      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): docker-compose or jvm processes not detected
+      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): $string not detected
       sleep 1
       continue
     fi
     for cpu_usage in $cpu_usages; do
       if (( $(echo "$cpu_usage >= $cpu_usage_threshold" | bc -l) )); then 
         cpu_usage_threshold_exceeded=1
-        echo -n $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): docker-compose and jvm cpu usage: 
+        echo -n $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): $string: 
         printf '%5.1f %5.1f %5.1f %5.1f %5.1f' $cpu_usages
         echo : alert threshold all \< $cpu_usage_threshold
         check_passed=0
@@ -36,14 +38,14 @@ check() {
     done
     if [ $cpu_usage_threshold_exceeded -eq 0 ]; then
       check_passed=$(expr $check_passed + 1)
-      echo -n $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): docker-compose and jvm cpu usage: 
+      echo -n $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): $string: 
       printf '%5.1f %5.1f %5.1f %5.1f %5.1f' $cpu_usages
       echo -n : alert threshold all \< $cpu_usage_threshold met 
       printf '%3d ' $check_passed 
       echo of $check_passed_threshold consecutive times
     fi
     if [ $check_passed -eq $check_passed_threshold ]; then
-      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): cpu usage check passed
+      echo $0: $(date '+%Y-%m-%d %H:%M:%S.%N'): $string check passed
       break
     fi
     sleep 1
